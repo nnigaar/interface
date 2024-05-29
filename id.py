7a6903eb35f1c602e25a3eed99f36f3bc0bd59c8
nnigaar <novruzlunigar1@gmail.com>
2024-05-29 16:48:14 +0400

    Update id.py

--- id.py
+++ id.py
@@ -1,16 +1,25 @@
 import requests
-wiki_api_url = "https://tr.wikipedia.org/w/api.php"
-page_title = "Albert Einstein"
 
-params = {
-    "action": "query",
-    "format": "json",
-    "titles": page_title,
-    "prop": "info",
-    "inprop": "url"
-}
-response = requests.get(url=wiki_api_url, params=params)
-data = response.json()
-page_id = list(data["query"]["pages"].keys())[0]
-print("Page ID for", page_title, ":", page_id)
+def get_wikipedia_content(page_title):
+    base_url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "action": "query",
+        "format": "json",
+        "prop": "extracts",
+        "titles": page_title,
+        "exintro": True,  # Get only the introductory section
+        "explaintext": True  # Get plain text instead of HTML
+    }
 
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    # Extract page content from the response
+    page_id = next(iter(data["query"]["pages"]))
+    content = data["query"]["pages"][page_id]["extract"]
+
+    return content
+
+# Fetch content of NASA Wikipedia page
+nasa_content = get_wikipedia_content("NASA")
+print(nasa_content)
